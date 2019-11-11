ca550fa25d56a2ed3e91314907a9ec233e1a0279
shareluck <1036596437@qq.com>
2019-11-11 23:28:57 +0800

    去掉claim页面

--- src/components/pages/index.vue
+++ src/components/pages/index.vue
@@ -18,7 +18,7 @@
             <el-menu-item index="/home">Home</el-menu-item>
             <el-submenu index="2">
                 <template slot="title">Warranty</template>
-                <el-menu-item index="/warranty/claim">Online Warranty Claim</el-menu-item>
+                <el-menu-item index="/warranty/claim/form">Online Warranty Claim</el-menu-item>
                 <!-- <el-menu-item index="/warranty/procedure">Warranty Claim Procedure</el-menu-item> -->
                 <el-menu-item index="/warranty/registration">Warranty Registration</el-menu-item>
                 <el-menu-item index="/warranty/extension">Warranty Extension</el-menu-item>
